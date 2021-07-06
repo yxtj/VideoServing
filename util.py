59249e1d42af2fc0a545a163cdd9628285332c47
Tian Zhou <yanxiangtianji@gmail.com>
2021-07-06 19:07:08 -0400

    add sample functions

--- util.py
+++ util.py
@@ -5,6 +5,8 @@ Created on Sun Feb  7 03:09:50 2021
 @author: yanxi
 """
 
+import numpy as np
+
 def compIoU(boxA, boxB):
     xA = max(boxA[0], boxB[0])
     yA = max(boxA[1], boxB[1])
@@ -32,3 +34,54 @@ def box_center(boxes):
     else:
         c = (boxes[:,:2]+boxes[:,2:])/2
     return c
+
+def sample_index(n, period, slength, pos='tail'):
+    assert period >= slength
+    assert pos in ['head', 'middle', 'tail']
+    nf_p = int(period)
+    nf_s = int(slength)
+    if pos == 'head':
+        ns = n // nf_p
+        if n % nf_p >= nf_s:
+            ns += 1
+        start = 0
+    elif pos == 'middle':
+        ns = n // nf_p
+        if n % nf_p >= (nf_p + nf_s)//2:
+            ns += 1
+        start = (nf_p - nf_s) // 2
+    else: # tail
+        ns = n // nf_p
+        start = nf_p - nf_s
+    res = []
+    for i in range(start, n, nf_p):
+        t = np.arange(i,i+nf_s)
+        if len(t) == nf_s: # in case the last period is not complete
+            res.append(t)
+    return np.array(res, int)
+
+def sample_data(data, period, slength, pos='tail'):
+    assert period >= slength
+    assert pos in ['head', 'middle', 'tail']
+    nf_p = int(period)
+    nf_s = int(slength)
+    n = len(data)
+    if pos == 'head':
+        ns = n // nf_p
+        if n % nf_p >= nf_s:
+            ns += 1
+        start = 0
+    elif pos == 'middle':
+        ns = n // nf_p
+        if n % nf_p >= (nf_p + nf_s)//2:
+            ns += 1
+        start = (nf_p - nf_s) // 2
+    else: # tail
+        ns = n // nf_p
+        start = nf_p - nf_s
+    res = []
+    for i in range(start, n, nf_p):
+        t = data[i:i+nf_s]
+        if len(t) == nf_s: # in case the last period is not complete
+            res.append(t)
+    return np.array(res)
