3460685049f7ad4b00ccd52b861f0530c7c6b953
Tian Zhou <yanxiangtianji@gmail.com>
2021-09-23 15:31:47 -0400

    [fix] some type bug

--- common.py
+++ common.py
@@ -1,34 +1,34 @@
 # -*- coding: utf-8 -*-
 
 #from collections import namedtuple
-import typing.NamedTuple
+from typing import NamedTuple
 import numpy as np
 
 #Configuration = namedtuple('Configuration', ['fps', 'rsl', 'roi', 'model'])
 
-class Configuration(typing.NamedTuple):
+class Configuration(NamedTuple):
     fps: int
     rsl: int
     #roi: bool
     #model: str
 
 #Task = namedtuple('Task', ['time', 'frame', 'jid', 'sid', 'fid', 'rs', 'fr'])
-class Task(typing.NamedTuple):
-    frame: np.ndarry
+class Task(NamedTuple):
+    frame: np.ndarray
     sid: int
     fid: int
     tag: int
     time: float
 
 #DetectionResult = namedtuple('DetectionResult', ['box', 'lbl'])
-class DetectionResult(typing.NamedTuple):
+class DetectionResult(NamedTuple):
     box: np.ndarray
     lbl: int
     time: float
 
 
 #TrackResult = namedtuple('TrackResult', ['id', 'center', 'box', 'lbl', 'speed'])
-class TrackResult(typing.NamedTuple):
+class TrackResult(NamedTuple):
     id: int
     box: np.ndarray
     lbl: int
