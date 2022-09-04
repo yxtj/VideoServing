ffa320a45c84b4b0487da48e90efa123dfcbe735
Tian Zhou <yanxiangtianji@gmail.com>
2022-09-04 00:56:27 -0400

    add certify conf

--- streamprocess.py
+++ streamprocess.py
@@ -1,20 +1,45 @@
 # -*- coding: utf-8 -*-
 
-import numpy as np
-from common import DetectionResult
+#import numpy as np
+from common import Configuration
+from videoholder import VideoHolder
+from track import Tracker
 
-class StreamProcessing():
+
+class CertifyConf:
+    def __init__(self, interval:float, length:float, conf:Configuration=None):
+        # sample interval
+        self.interval = interval
+        # sample length
+        self.length = length
+        # configuration used to certify
+        self.conf = conf
+
+class RefineConf:
+    def __init__(self, bound:float, length:float, conf:Configuration=None):
+        # threshold for difference
+        self.bound = bound
+        # length to refine
+        self.length = length
+        # configuration used to refine
+        self.conf = conf
+
+
+# %% stream processor
+
+class StreamProcessor():
     
-    def __init__(self, sid, knobs, f_post, cinterval=30, clength=1, rbound=0.3,
-                 track_mthd='sort', cconf=None, rconf=None, **kwargs):
-        self.sid = sid
-        #self.knobs = knobs
-        self.knob_fps = knobs['fps']
-        self.knob_rsl = knobs['rsl']
-        self.cinterval = cinterval # certify - sample interval
-        self.clength = clength # certify - sample length
-        self.rbound = rbound # refine - threshold
-        self.set_tracker(track_mthd, **kwargs)
+    def __init__(self, streamid, video:VideoHolder, 
+                 knobs:list[Configuration], tracker:Tracker,
+                 ctf_conf:CertifyConf(30, 1, None), 
+                 rfn_conf:RefineConf(0.3, 30, None)):
+        self.streamid = streamid
+        self.knobs = knobs
+        self.knob_fps = [c.fps for c in self.knobs]
+        self.knob_rsl = [c.rsl for c in self.knobs]
+        self.tracker = tracker
+        self.ctf_conf = ctf_conf
+        self.rfn_conf = rfn_conf
         # buffers
         self.bfFrame = []
         self.bfTask = []
@@ -26,18 +51,7 @@ class StreamProcessing():
         self.srsl = None
         # intermediate
         self.fid = 0
-        self.fid_
     
-    def set_tracker(self, track_mthd, **kwargs):
-        assert track_mthd in ['center', 'sort']
-        if track_mthd == 'center':
-            from track.centroidtracker import CentroidTracker
-            self.tracker = CentroidTracker(kwargs['tck_age'])
-        elif track_mthd == 'sort':
-            from track.sorttracker import SortTracker
-            self.tracker = SortTracker(kwargs['tck_age'], kwargs['tck_age'], 
-                                       iou_threshold=kwargs['tck_min_iou'])
-                
     def task_frame(self, frame):
         pass
     
